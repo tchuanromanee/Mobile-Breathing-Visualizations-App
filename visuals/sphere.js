da24941da994eae37352cd7db31e175c278f3e98
tchuanromanee <tchuanromanee@yahoo.com>
2019-02-22 14:30:36 -0500

    Basic sphere representation: continuous transition

--- visuals/sphere.js
+++ visuals/sphere.js
@@ -1,23 +1,47 @@
 var circleDemo = d3.select("#myCircle")
-circleDemo.attr("r", 40);
+circleDemo.attr("r", 10);
 circleDemo.style("stroke", "black");
 circleDemo.style("fill", "steelblue"); 
 circleDemo.attr("cx", 50)
     .attr("cy", 50).on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-        .on("mousedown", animateFirstStep);
+        .on("mousedown", sphereVisualizationStart);
 
-function animateFirstStep(){
+var timer = d3.timer(function(t) { // callback function
+  if (t > 30000) timer.stop();  // run for 5 seconds
+  d3.select("p").html(t);       // update some text
+});
+
+function sphereVisualizationStart(){
     d3.select(this)
-      .transition()            
+      .transition()
         .delay(0)            
-        .duration(1000)
+        .duration(6000)
+        .attr("r", 40)
+		.style("fill", "green")
+        //second transition
+		.transition()
+		.delay(30)
+        .duration(6000)
         .attr("r", 10)
-        .each("end", animateSecondStep);
+		.style("fill", "red")
+		.on("end", sphereVisualizationStart);
 };
 
-function animateSecondStep(){
-    d3.select(this)
-		.transition()
-        .duration(1000)
-        .attr("r", 40);
+function circleTransition() { 
+
+    repeat();
+    
+    function repeat() {
+      timeCircle
+        .attr('cx', 40)      // position the circle at 40 on the x axis
+        .attr('cy', 250)     // position the circle at 250 on the y axis
+        .transition()        // apply a transition
+        .duration(2000)      // apply it over 2000 milliseconds
+        .attr('cx', 920)     // move the circle to 920 on the x axis
+        .transition()        // apply a transition
+        .duration(2000)      // apply it over 2000 milliseconds
+        .attr('cx', 40)      // return the circle to 40 on the x axis
+        .on("end", repeat);  // when the transition finishes start again
+    };
+
 };
