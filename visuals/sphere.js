9c4fb945fac437540776df9d93593b8ffc447ec0
tchuanromanee <tchuanromanee@yahoo.com>
2019-02-22 15:24:34 -0500

    Set breath phases as functions and remove extraneous variables

--- visuals/sphere.js
+++ visuals/sphere.js
@@ -1,47 +1,64 @@
-var circleDemo = d3.select("#myCircle")
-circleDemo.attr("r", 10);
-circleDemo.style("stroke", "black");
-circleDemo.style("fill", "steelblue"); 
-circleDemo.attr("cx", 50)
+var circleRep = d3.select("#myCircle")
+circleRep.attr("r", 10);
+circleRep.style("fill", "steelblue"); 
+circleRep.attr("cx", 50)
     .attr("cy", 50).on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-        .on("mousedown", sphereVisualizationStart);
+        .on("mousedown", sphereVisualizationIn);
 
-var timer = d3.timer(function(t) { // callback function
-  if (t > 30000) timer.stop();  // run for 5 seconds
-  d3.select("p").html(t);       // update some text
-});
+var totalTimems = 30000;
 
-function sphereVisualizationStart(){
+var phase = "Not Started";
+
+d3.select("p").html("Phase: "+ phase);       // update some text
+
+
+function sphereVisualizationIn(){
+	phase = "Inhale";
+	d3.select("p").html("Phase: " + phase);
     d3.select(this)
+		.style("fill", "green");
+	d3.select(this)
       .transition()
         .delay(0)            
-        .duration(6000)
+        .duration(4000)
         .attr("r", 40)
-		.style("fill", "green")
-        //second transition
+		.on("end", sphereVisualizationHold);
+};
+
+function sphereVisualizationHold() {
+	phase = "Hold";
+	d3.select("p").html("Phase: " + phase);
+	d3.select(this).style("fill", "yellow");
+	console.log(d3.select(this).attr("r"));
+	if (d3.select(this).attr("r") == 40) {
+		d3.select(this).transition().delay(4000).on("end",sphereVisualizationOut);
+	}
+	else {
+		d3.select(this).transition().delay(4000).on("end",sphereVisualizationIn);
+	}
+}
+
+function sphereVisualizationOut(){
+	phase = "Exhale";
+	d3.select("p").html("Phase: " + phase);
+     d3.select(this)
+		.style("fill", "red");
+	d3.select(this)
 		.transition()
-		.delay(30)
-        .duration(6000)
+        .duration(4000)
         .attr("r", 10)
-		.style("fill", "red")
-		.on("end", sphereVisualizationStart);
+		.on("end", sphereVisualizationHold);
 };
 
-function circleTransition() { 
-
-    repeat();
-    
-    function repeat() {
-      timeCircle
-        .attr('cx', 40)      // position the circle at 40 on the x axis
-        .attr('cy', 250)     // position the circle at 250 on the y axis
-        .transition()        // apply a transition
-        .duration(2000)      // apply it over 2000 milliseconds
-        .attr('cx', 920)     // move the circle to 920 on the x axis
-        .transition()        // apply a transition
-        .duration(2000)      // apply it over 2000 milliseconds
-        .attr('cx', 40)      // return the circle to 40 on the x axis
-        .on("end", repeat);  // when the transition finishes start again
-    };
-
-};
+function sphereVisualizationOutHold() {
+	phase = "Hold";
+	d3.select("p").html("Phase: " + phase);
+	d3.select(this).style("fill", "yellow");
+	var holdTimer = d3.timer(function(t) { // callback function
+		  if (t > 4000) {
+			  
+			  timer.stop();  // run for 4 seconds
+			  sphereVisualizationIn(); // breathe out
+		  }
+		});
+}
