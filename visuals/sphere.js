69b420ac25637aeddfdc6e07073b7edf6e66a4a3
tchuanromanee <tchuanromanee@yahoo.com>
2019-02-22 15:37:21 -0500

    Set a limit for how many breathing phases there will be

--- visuals/sphere.js
+++ visuals/sphere.js
@@ -6,6 +6,9 @@ circleRep.attr("cx", 50)
     .on("mousedown", sphereVisualizationIn);
 
 var totalTimems = 30000;
+var numPhases = 0;
+var phaseDurationms = 4000; // 4 sec per phase
+var totalNumPhases = totalTimems / phaseDurationms;
 
 var phase = "Tap to Start";
 
@@ -13,6 +16,10 @@ d3.select("p").html(phase);       // update phase text
 
 
 function sphereVisualizationIn(){
+	numPhases++;
+	if (numPhases >= totalNumPhases) {
+		return;
+	}
 	phase = "Inhale";
 	d3.select("p").html(phase);
     d3.select(this)
@@ -20,32 +27,34 @@ function sphereVisualizationIn(){
 	d3.select(this)
       .transition()
         .delay(0)            
-        .duration(4000)
+        .duration(phaseDurationms)
         .attr("r", 40)
 		.on("end", sphereVisualizationHold);
 };
 
 function sphereVisualizationHold() {
+	numPhases++;
 	phase = "Hold";
 	d3.select("p").html(phase);
 	d3.select(this).style("fill", "yellow");
 	console.log(d3.select(this).attr("r"));
 	if (d3.select(this).attr("r") == 40) {
-		d3.select(this).transition().delay(4000).on("end",sphereVisualizationOut);
+		d3.select(this).transition().delay(phaseDurationms).on("end",sphereVisualizationOut);
 	}
 	else {
-		d3.select(this).transition().delay(4000).on("end",sphereVisualizationIn);
+		d3.select(this).transition().delay(phaseDurationms).on("end",sphereVisualizationIn);
 	}
 }
 
 function sphereVisualizationOut(){
+	numPhases++;
 	phase = "Exhale";
 	d3.select("p").html(phase);
      d3.select(this)
 		.style("fill", "red");
 	d3.select(this)
 		.transition()
-        .duration(4000)
+        .duration(phaseDurationms)
         .attr("r", 10)
 		.on("end", sphereVisualizationHold);
 };
