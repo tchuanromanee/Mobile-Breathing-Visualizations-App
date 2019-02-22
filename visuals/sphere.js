97af6064a799257e8083097e154af263782d81b7
tchuanromanee <tchuanromanee@yahoo.com>
2019-02-22 15:25:26 -0500

    Update text for phase and remove extra function:

--- visuals/sphere.js
+++ visuals/sphere.js
@@ -7,14 +7,14 @@ circleRep.attr("cx", 50)
 
 var totalTimems = 30000;
 
-var phase = "Not Started";
+var phase = "Tap to Start";
 
-d3.select("p").html("Phase: "+ phase);       // update some text
+d3.select("p").html(phase);       // update phase text
 
 
 function sphereVisualizationIn(){
 	phase = "Inhale";
-	d3.select("p").html("Phase: " + phase);
+	d3.select("p").html(phase);
     d3.select(this)
 		.style("fill", "green");
 	d3.select(this)
@@ -27,7 +27,7 @@ function sphereVisualizationIn(){
 
 function sphereVisualizationHold() {
 	phase = "Hold";
-	d3.select("p").html("Phase: " + phase);
+	d3.select("p").html(phase);
 	d3.select(this).style("fill", "yellow");
 	console.log(d3.select(this).attr("r"));
 	if (d3.select(this).attr("r") == 40) {
@@ -40,7 +40,7 @@ function sphereVisualizationHold() {
 
 function sphereVisualizationOut(){
 	phase = "Exhale";
-	d3.select("p").html("Phase: " + phase);
+	d3.select("p").html(phase);
      d3.select(this)
 		.style("fill", "red");
 	d3.select(this)
@@ -49,16 +49,3 @@ function sphereVisualizationOut(){
         .attr("r", 10)
 		.on("end", sphereVisualizationHold);
 };
-
-function sphereVisualizationOutHold() {
-	phase = "Hold";
-	d3.select("p").html("Phase: " + phase);
-	d3.select(this).style("fill", "yellow");
-	var holdTimer = d3.timer(function(t) { // callback function
-		  if (t > 4000) {
-			  
-			  timer.stop();  // run for 4 seconds
-			  sphereVisualizationIn(); // breathe out
-		  }
-		});
-}
