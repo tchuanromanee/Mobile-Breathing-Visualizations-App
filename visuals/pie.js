8022197371616ead8fe6b43a5e5d9bf0cc030362
tchuanromanee <tchuanromanee@yahoo.com>
2019-02-27 14:18:46 -0500

    Linear transition easing for arctween

--- visuals/pie.js
+++ visuals/pie.js
@@ -7,19 +7,18 @@ var width = 960,
     height = 500,
 	innerRadiusArc = 50,
 	outerRadiusArc = 70;
+
 // Draw inhale arc	
 var inhaleArc = d3.arc()
     .innerRadius(innerRadiusArc)
     .outerRadius(outerRadiusArc)
-    .startAngle(0); //converting from degs to radians
+    .startAngle(0); 
 
-  var arc = d3.arc()
-    .innerRadius(innerRadiusArc)
-    .outerRadius(outerRadiusArc)
-    .startAngle(0);
-
-
-	  
+var arc = d3.arc()
+	.innerRadius(innerRadiusArc)
+	.outerRadius(outerRadiusArc)
+	.startAngle(0);
+  
 var inhaleArcElem = svgContainer.append("path")	  
     .datum({endAngle: Math.PI/2})
     .attr("transform", "translate(100,100)")
@@ -39,7 +38,6 @@ var inhaleHoldArcElem = svgContainer.append("path")
     .attr("d", inhaleHoldArc)
     .attr("transform", "translate(100,100)")
 	.style("fill", "yellow");
-	
 
 // Draw exhale arc	
 var exhaleArc = d3.arc()
@@ -67,7 +65,7 @@ var exhaleHoldArcElem = svgContainer.append("path")
 	
 var overlayArcElem = svgContainer.append("path")
     .datum({endAngle: 0})
-    .style("fill", "green")
+    .style("fill", "#EEEEEE")
 	.style("stroke", "black")
 	.attr("transform", "translate(100,100)")
 	//.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
@@ -90,6 +88,9 @@ function inhale(){
 		return;
 	}
 	if (numPhases % 4 == 1) {
+		if (numPhases > 1) {
+			overlayArcElem.remove();
+		}
 		phase = "Inhale";
 	}
 	else if (numPhases % 4 == 2) {
@@ -103,8 +104,9 @@ function inhale(){
 	}
 	d3.select("p").html(phase);
 	overlayArcElem.transition()
-      .duration(phaseDurationms)
-      .attrTween("d", arcTween(numPhases * Math.PI/2))
+		.ease(d3.easeLinear)
+      .duration(16000)
+      .attrTween("d", arcTween(2 * Math.PI))
 	  .on("end", inhale);
 	 
 };
@@ -113,47 +115,12 @@ function inhale(){
 // arcs from their current angle to the specified new angle.
 function arcTween(newAngle) {
 
-  // The function passed to attrTween is invoked for each selected element when
-  // the transition starts, and for each element returns the interpolator to use
-  // over the course of transition. This function is thus responsible for
-  // determining the starting angle of the transition (which is pulled from the
-  // element’s bound datum, d.endAngle), and the ending angle (simply the
-  // newAngle argument to the enclosing function).
   return function(d) {
 
-    // To interpolate between the two angles, we use the default d3.interpolate.
-    // (Internally, this maps to d3.interpolateNumber, since both of the
-    // arguments to d3.interpolate are numbers.) The returned function takes a
-    // single argument t and returns a number between the starting angle and the
-    // ending angle. When t = 0, it returns d.endAngle; when t = 1, it returns
-    // newAngle; and for 0 < t < 1 it returns an angle in-between.
     var interpolate = d3.interpolate(d.endAngle, newAngle);
 
-    // The return value of the attrTween is also a function: the function that
-    // we want to run for each tick of the transition. Because we used
-    // attrTween("d"), the return value of this last function will be set to the
-    // "d" attribute at every tick. (It’s also possible to use transition.tween
-    // to run arbitrary code for every tick, say if you want to set multiple
-    // attributes from a single function.) The argument t ranges from 0, at the
-    // start of the transition, to 1, at the end.
     return function(t) {
-
-      // Calculate the current arc angle based on the transition time, t. Since
-      // the t for the transition and the t for the interpolate both range from
-      // 0 to 1, we can pass t directly to the interpolator.
-      //
-      // Note that the interpolated angle is written into the element’s bound
-      // data object! This is important: it means that if the transition were
-      // interrupted, the data bound to the element would still be consistent
-      // with its appearance. Whenever we start a new arc transition, the
-      // correct starting angle can be inferred from the data.
       d.endAngle = interpolate(t);
-
-      // Lastly, compute the arc path given the updated data! In effect, this
-      // transition uses data-space interpolation: the data is interpolated
-      // (that is, the end angle) rather than the path string itself.
-      // Interpolating the angles in polar coordinates, rather than the raw path
-      // string, produces valid intermediate arcs during the transition.
       return arc(d);
     };
   };
